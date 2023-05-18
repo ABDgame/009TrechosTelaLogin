f2114b96c20abe19fc3311fa4519c3ab6375054b
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-05-18 20:36:08 -0300

    Update 03_Var_Let_e_Const.js

--- Trechos_JavaScript/Trechos_JavaScript-main/03_Var_Let_e_Const.js
+++ Trechos_JavaScript/Trechos_JavaScript-main/03_Var_Let_e_Const.js
@@ -1,1 +1,13 @@
+"use strict" 
 
+function teste(){
+    var nome="Débora"
+    if(true){
+        console.log("dentro do IF do teste: " + nome)
+    }
+    console.log("dentro do teste: " + nome)
+}
+
+teste()
+
+console.log("fora de teste: " + nome)
