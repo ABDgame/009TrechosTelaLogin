2db884655e1549daad2e361ed8562a3983b90afb
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-05-20 21:57:18 -0300

    Update 07_Operadores_Logicos.js

--- Trechos_JavaScript/Trechos_JavaScript-main/07_Operadores_Logicos.js
+++ Trechos_JavaScript/Trechos_JavaScript-main/07_Operadores_Logicos.js
@@ -1,1 +1,39 @@
+/*
+&& -> and e
+|| -> or ou
+!  -> not não
 
+
+1° Exemplo
+let numero1,numero2,numero3,numero4
+numero1=1000
+numero2=500
+numero3=1500
+numero4=200
+console.log((n1>n2)&&(n1>n3))
+
+
+2° Exemplo
+let numero1,numero2,numero3,numero4
+numero1=1000
+numero2=500
+numero3=1500
+numero4=200
+console.log((n1>n2)||(n1>n3))
+
+3° Exemplo
+let numero1,numero2,numero3,numero4
+numero1=1000
+numero2=500
+numero3=1500
+numero4=200
+
+console.log(!((n1>n2)||(n1>n3)))
+*/
+let numero1,numero2,numero3,numero4
+numero1=1000
+numero2=500
+numero3=1500
+numero4=200
+if(numero1>numero2){
+    console.log(!((n1>n2)||(n1>n3)))
