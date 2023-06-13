f29a424d9605c04815e4914b6a9b3fe4957d6fe4
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-06-13 19:47:25 -0300

    Update 003-Select-Where-And-Or.sql

--- Trechos_MySQL/DataWithMo/003-Select-Where-And-Or.sql
+++ Trechos_MySQL/DataWithMo/003-Select-Where-And-Or.sql
@@ -10,3 +10,17 @@ FROM funcionarios;
 
 SELECT funcionario_id,data_admissao,salario_funcionario 
 FROM funcionarios;
+
+--WHERE clause + AND & OR
+
+-- Select only the funcionarios who make more than 50k
+SELECT =
+FROM funcionarios 
+WHERE salario_funcionario > 50000;
+
+-- Select only the funcionarios who work em São Paulo  coffeshop
+SELECT =
+FROM funcionarios 
+WHERE  coffeshop_id = 1;
+
+-- Select todos os funcionários que trabalham em São Paulo  e 
