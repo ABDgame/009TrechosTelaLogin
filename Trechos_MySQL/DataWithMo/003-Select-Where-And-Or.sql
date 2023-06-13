efc3c3941ccb1ac300e80075ecb1be82746d6b96
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-06-13 19:33:58 -0300

    Update 003-Select-Where-And-Or.sql

--- Trechos_MySQL/DataWithMo/003-Select-Where-And-Or.sql
+++ Trechos_MySQL/DataWithMo/003-Select-Where-And-Or.sql
@@ -1,1 +1,12 @@
+-- SELECT statement
 
+SELECT FROM funcionários;
+SELECT FROM lojas;
+SELECT FROM locations;
+SELECT FROM fornecedores;
+
+SELECT funcionario_id,nome_funcionario, sobrenome_funcionario
+FROM funcionarios;
+
+SELECT funcionario_id,data_admissao,salario_funcionario 
+FROM funcionarios;
