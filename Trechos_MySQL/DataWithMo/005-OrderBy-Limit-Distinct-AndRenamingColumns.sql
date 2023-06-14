2e45f7a38e112a927bb7fdb8992078279ca9eb2b
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-06-14 20:25:09 -0300

    Update 005-OrderBy-Limit-Distinct-AndRenamingColumns.sql

--- Trechos_MySQL/DataWithMo/005-OrderBy-Limit-Distinct-AndRenamingColumns.sql
+++ Trechos_MySQL/DataWithMo/005-OrderBy-Limit-Distinct-AndRenamingColumns.sql
@@ -1,1 +1,17 @@
 
+SELECT
+FROM
+WHERE email IS NULL;
+
+SELECT
+FROM
+WHERE NOT email IS NULL;
+
+SELECT
+FROM
+WHERE salario_funcionario BETWEEN 35000 AND 50000 ;
+
+SELECT
+FROM
+WHERE salario_funcionario >= 35000
+AND salario_funcionario <= 50000;
