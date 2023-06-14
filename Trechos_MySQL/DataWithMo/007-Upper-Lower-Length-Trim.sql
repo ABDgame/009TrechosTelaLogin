0de8b586a4b339d18b7b3bfe3fd0ce2c64120109
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-06-14 20:41:15 -0300

    Update 007-Upper-Lower-Length-Trim.sql

--- Trechos_MySQL/DataWithMo/007-Upper-Lower-Length-Trim.sql
+++ Trechos_MySQL/DataWithMo/007-Upper-Lower-Length-Trim.sql
@@ -1,1 +1,20 @@
+-- UPPER,LOWER,LENGTH,TRIM
 
+-- Uppercase nome e sobrenome
+SELECT 
+
+   UPPER AS
+
+   UPPER AS
+FROM
+
+
+SELECT 
+
+    LOWER nome_funcionario AS sobrenome_funcionario,
+    
+    LOWER         AS
+FROM      ;
+
+--
+SELECT
