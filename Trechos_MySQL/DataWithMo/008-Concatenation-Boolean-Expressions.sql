e54fad0124459b74b7fd6b96c49b700fa63ab879
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-06-14 20:53:24 -0300

    Update 008-Concatenation-Boolean-Expressions.sql

--- Trechos_MySQL/DataWithMo/008-Concatenation-Boolean-Expressions.sql
+++ Trechos_MySQL/DataWithMo/008-Concatenation-Boolean-Expressions.sql
@@ -1,1 +1,18 @@
 
+SELECT 
+
+      LOWER      AS
+      
+      LOWER      AS
+FROM 
+
+
+SELECT
+
+     LENGTH        AS
+FROM
+
+SELECT
+     LENGTH        AS
+     LENGTH        AS
+     LENGTH TRIM     AS
