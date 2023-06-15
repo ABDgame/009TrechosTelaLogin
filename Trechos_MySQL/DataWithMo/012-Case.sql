b5a8dd9674787ef4ab35b43b262366bf410e1fb0
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-06-14 21:23:58 -0300

    Update 012-Case.sql

--- Trechos_MySQL/DataWithMo/012-Case.sql
+++ Trechos_MySQL/DataWithMo/012-Case.sql
@@ -1,1 +1,17 @@
+GROUP BY
+HAVING COUNT 
+ORDER BY      DESC;
 
+
+
+SELECT
+
+     COUNT    AS
+              AVG     AS
+     MIN      AS
+     MAX      AS
+     SUM      AS
+FROM
+GROUP BY
+HAVING MIN
+ORDER BY       DESC;
